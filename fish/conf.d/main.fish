4105b53444157fd00999346cc978c874eef54347
dcfrankel <frankelcdavid@gmail.com>
2025-08-08 14:12:10 -0400

    Update theme

--- fish/conf.d/main.fish
+++ fish/conf.d/main.fish
@@ -3,7 +3,7 @@ fish_vi_key_bindings
 
 # Prompt and theme
 fish_config prompt choose nim
-fish_config theme choose "Base16 Eighties"
+fish_config theme choose "ayu Mirage"
 
 # Path stuff
 fish_add_path /bin
