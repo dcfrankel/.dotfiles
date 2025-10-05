912dde0fb97e7349aad0a7a66b634721c8f94eff
dcfrankel <frankelcdavid@gmail.com>
2025-10-05 14:58:16 -0400

    Use rose pine color theme

--- nvim/lua/pack.lua
+++ nvim/lua/pack.lua
@@ -2,18 +2,22 @@ local pack = {}
 
 local function configure()
     -- Telescope keymaps
-    local builtin = require('telescope.builtin')
-    vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-    vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-    vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-    vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+    local telescope_builtin = require('telescope.builtin')
+    vim.keymap.set('n', '<leader>ff', telescope_builtin.find_files, { desc = 'Telescope find files' })
+    vim.keymap.set('n', '<leader>fg', telescope_builtin.live_grep, { desc = 'Telescope live grep' })
+    vim.keymap.set('n', '<leader>fb', telescope_builtin.buffers, { desc = 'Telescope buffers' })
+    vim.keymap.set('n', '<leader>fh', telescope_builtin.help_tags, { desc = 'Telescope help tags' })
+
+    -- Set theme
+    vim.cmd("colorscheme rose-pine")
 end
 
 function pack.load()
     vim.pack.add({
         -- Required by telescope
         { src = "https://github.com/nvim-lua/plenary.nvim" },
-        { src = "https://github.com/nvim-telescope/telescope.nvim", version = "0.1.8" }
+        { src = "https://github.com/nvim-telescope/telescope.nvim", version = "0.1.8" },
+        { src = "https://github.com/rose-pine/neovim" },
     })
     -- Load various plugin configurations
     configure()
