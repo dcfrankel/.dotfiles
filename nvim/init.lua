ad597ee9fb015caeb4b081c2892f763fe413ebcb
dcfrankel <frankelcdavid@gmail.com>
2025-10-11 11:20:52 -0400

    Add keymap descriptions and fix formatting

--- nvim/init.lua
+++ nvim/init.lua
@@ -81,16 +81,16 @@ vim.diagnostic.config({virtual_lines = true})
 -- Specific setup actions for LSP buffers
 vim.api.nvim_create_autocmd('LspAttach', {
   callback = function(ev)
-    local bufnr = ev.buf -- Get the buffer
-    local client = vim.lsp.get_client_by_id(ev.data.client_id) -- Get the LSP client
+    local bufnr = ev.buf -- Get the buffer local
+    client = vim.lsp.get_client_by_id(ev.data.client_id) -- Get the LSP client
     if client:supports_method('textDocument/completion') then
-      -- Enable native completion using LSP
-      vim.lsp.completion.enable(true, client.id, bufnr, { autotrigger = true })
+        -- Enable native completion using LSP
+        vim.lsp.completion.enable(true, client.id, bufnr, { autotrigger = true })
     -- Configure gd and gD to act as you'd expect
-    vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = bufnr, silent = true })
-    vim.keymap.set("n", "gD", vim.lsp.buf.definition, { buffer = bufnr, silent = true })
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = bufnr, silent = true, desc = "Go to definition" })
+    vim.keymap.set("n", "gD", vim.lsp.buf.definition, { buffer = bufnr, silent = true, desc = "Go to definition" })
     -- Use the same code action binding as Zed
-    vim.keymap.set("n", "g.", vim.lsp.buf.code_action, { buffer = bufnr, silent = true })
+    vim.keymap.set("n", "g.", vim.lsp.buf.code_action, { buffer = bufnr, silent = true, desc = "Open code action menu" })
     end
   end,
 })
