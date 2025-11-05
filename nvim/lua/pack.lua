9b42e8950f1895c48a57c851b49e11c84ba3d251
dcfrankel <frankelcdavid@gmail.com>
2025-11-05 17:22:57 -0500

    Use catppuccin theme

--- nvim/lua/pack.lua
+++ nvim/lua/pack.lua
@@ -1,11 +1,14 @@
 local M = {}
 
 function M.load_all()
+  -- Load some themes
   vim.pack.add({
     { src = "https://github.com/rose-pine/neovim", name = "rose-pine" },
+    { src = "https://github.com/rebelot/kanagawa.nvim", name = "kanagawa" },
+    { src = "https://github.com/catppuccin/nvim", name = "catppuccin" }
   })
   -- Set theme
-  vim.cmd("colorscheme rose-pine")
+  vim.cmd("colorscheme catppuccin")
 
   -- Load various plugin configurations
   require('plugin_configs.miniclue').setup()
