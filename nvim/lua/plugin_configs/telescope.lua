8dc2dd3dc6babf93d5976b1aa098ad7d5fece2b0
dcfrankel <frankelcdavid@gmail.com>
2025-11-06 20:52:21 -0500

    Update telescope keybindings

--- nvim/lua/plugin_configs/telescope.lua
+++ nvim/lua/plugin_configs/telescope.lua
@@ -10,10 +10,10 @@ function M.setup()
 
   -- Telescope keymaps
   local telescope_builtin = require('telescope.builtin')
-  vim.keymap.set('n', '<leader>ff', telescope_builtin.find_files, { desc = 'Telescope find files' })
-  vim.keymap.set('n', '<leader>fg', telescope_builtin.live_grep, { desc = 'Telescope live grep' })
-  vim.keymap.set('n', '<leader>fb', telescope_builtin.buffers, { desc = 'Telescope buffers' })
-  vim.keymap.set('n', '<leader>fh', telescope_builtin.help_tags, { desc = 'Telescope help tags' })
+  vim.keymap.set('n', '<leader>tf', telescope_builtin.find_files, { desc = 'Telescope find files' })
+  vim.keymap.set('n', '<leader>tg', telescope_builtin.live_grep, { desc = 'Telescope live grep' })
+  vim.keymap.set('n', '<leader>tb', telescope_builtin.buffers, { desc = 'Telescope buffers' })
+  vim.keymap.set('n', '<leader>th', telescope_builtin.help_tags, { desc = 'Telescope help tags' })
 end
 
 return M
