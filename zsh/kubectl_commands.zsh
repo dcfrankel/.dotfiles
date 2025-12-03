91cd756fcc79b4fd375c3f29e33a3a68b36a0cbb
dcfrankel <frankelcdavid@gmail.com>
2025-12-03 10:52:08 -0500

    Add find-pods kubectl command

--- zsh/kubectl_commands.zsh
+++ zsh/kubectl_commands.zsh
@@ -2,9 +2,16 @@
 ## A sane set of default overrides and customizations for the kubectl CLI
 
 # Add shared commands defaults to the list
-SHARED_KUBECTL_CLI_SUFFIX=(--all-namespaces)
+SHARED_KUBECTL_CLI_SUFFIX=(--all-namespaces -o wide)
 
 # List pods with specific label
 function k-pod-labels() {
   kubectl get pods -l $1 ${SHARED_KUBECTL_CLI_SUFFIX[@]}
+}
+
+# List pods with a given name
+function k-find-pods() {
+  # Makes sure to keeper table header in output
+  TABLE_HEADER="NAME"
+  kubectl get pods ${SHARED_KUBECTL_CLI_SUFFIX[@]} | grep --color=never -e "$TABLE_HEADER" -e $1
 }
