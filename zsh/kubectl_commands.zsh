595c28ee8912ab09ac8e1075b4de0d1ea88f6625
David Frankel <57853028+dcfrankel@users.noreply.github.com>
2026-01-21 09:09:24 -0500

    Add function to get kubectl object fields (#12)

--- zsh/kubectl_commands.zsh
+++ zsh/kubectl_commands.zsh
@@ -2,7 +2,7 @@
 ## A sane set of default overrides and customizations for the kubectl CLI
 
 # Add shared commands defaults to the list
-SHARED_KUBECTL_CLI_SUFFIX=(--all-namespaces -o wide)
+SHARED_KUBECTL_CLI_SUFFIX=(--all-namespaces -o=wide)
 
 # List pods with specific label
 function k-pod-labels() {
@@ -14,4 +14,9 @@ function k-find-pods() {
   # Makes sure to keeper table header in output
   TABLE_HEADER="NAME"
   kubectl get pods ${SHARED_KUBECTL_CLI_SUFFIX[@]} | grep --color=never -e "$TABLE_HEADER" -e $1
+}
+
+# Get an objects fields
+function k-object-fields() {
+  kubectl explain $1 --recursive=true
 }
