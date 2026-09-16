38eee51586e05e599af4c0eea57aa0c0873b52a2
dcfrankel <frankelcdavid@gmail.com>
2026-09-16 09:03:51 -0400

    Add explicit version pinning

--- nvim/lua/pack.lua
+++ nvim/lua/pack.lua
@@ -5,25 +5,25 @@ function M.load_all()
   -- configures its plugin; installation/versioning lives here.
   vim.pack.add({
     -- Themes
-    { src = "https://github.com/rose-pine/neovim", name = "rose-pine" },
-    { src = "https://github.com/rebelot/kanagawa.nvim", name = "kanagawa" },
-    { src = "https://github.com/catppuccin/nvim", name = "catppuccin" },
+    { src = "https://github.com/rose-pine/neovim", version = "ff483051a47e27d84bdef47703538df1ed9f4a47", name = "rose-pine" },
+    { src = "https://github.com/rebelot/kanagawa.nvim", version = "bb85e4bfc8d89b0e62c8fa53ccdd13d12e2f77b3", name = "kanagawa" },
+    { src = "https://github.com/catppuccin/nvim", version = "edefef779ab08ce1a4a404713e3012b0d202bd35", name = "catppuccin" },
 
     -- Fuzzy finder + its dependency
-    { src = "https://github.com/nvim-telescope/telescope.nvim", version = "0.1.9", name = "telescope" },
-    { src = "https://github.com/nvim-lua/plenary.nvim", name = "plenary" },
+    { src = "https://github.com/nvim-telescope/telescope.nvim", version = "84b9ba066d1860f7a586ce9cd732fd6c4f77d1d9", name = "telescope" },
+    { src = "https://github.com/nvim-lua/plenary.nvim", version = "74b06c6c75e4eeb3108ec01852001636d85a932b", name = "plenary" },
 
-    -- Treesitter (main branch: new setup/install API)
-    { src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "main", name = "nvim-treesitter" },
+    -- Treesitter (pinned snapshot of main: new setup/install API)
+    { src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "9a168f6357ed21c3a636e1727bc7d382abc451b8", name = "nvim-treesitter" },
 
     -- Editing / navigation helpers
-    { src = "https://github.com/ggandor/leap.nvim", name = "leap" },
-    { src = "https://github.com/windwp/nvim-autopairs", name = "nvim-autopairs" },
-    { src = "https://github.com/lewis6991/gitsigns.nvim", name = "gitsigns" },
-    { src = "https://github.com/nvim-mini/mini.clue", version = "stable", name = "mini.clue" },
+    { src = "https://github.com/ggandor/leap.nvim", version = "cbb7d3e4d9584d6b1ae47932f894eb4a16197a52", name = "leap" },
+    { src = "https://github.com/windwp/nvim-autopairs", version = "430522f95fe4fb7c511ec64f8c1a90cc6a66c05c", name = "nvim-autopairs" },
+    { src = "https://github.com/lewis6991/gitsigns.nvim", version = "fd36f038e52ad8409fbf9926ae4a0a514cca04d8", name = "gitsigns" },
+    { src = "https://github.com/nvim-mini/mini.clue", version = "62e9e38c3dc4b7d429f1b6e28d96f96cdff71132", name = "mini.clue" },
 
     -- Language-specific
-    { src = "https://github.com/mfussenegger/nvim-jdtls", name = "nvim-jdtls" },
+    { src = "https://github.com/mfussenegger/nvim-jdtls", version = "6e9d953f0b82bccdb834cfde0e893f3119c22592", name = "nvim-jdtls" },
   })
 
   -- Set theme
