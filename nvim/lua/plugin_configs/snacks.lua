6f58703e7b3751e5580cfe65f643ea55d9620f5d
dcfrankel <frankelcdavid@gmail.com>
2026-09-24 10:08:01 -0400

    Add support for git permalink copying

--- nvim/lua/plugin_configs/snacks.lua
+++ nvim/lua/plugin_configs/snacks.lua
@@ -1,5 +1,27 @@
 local M = {}
 
+-- Map the CarGurus GitHub Enterprise host to the same URL shape as github.com
+local gitbrowse_url_patterns = {
+  ["code%.cargurus%.com"] = {
+    branch = "/tree/{branch}",
+    file = "/blob/{branch}/{file}#L{line_start}-L{line_end}",
+    permalink = "/blob/{commit}/{file}#L{line_start}-L{line_end}",
+    commit = "/commit/{commit}",
+  },
+}
+
+local function copy_git_link()
+  Snacks.gitbrowse({
+    what = "permalink",
+    notify = false,
+    url_patterns = gitbrowse_url_patterns,
+    open = function(url)
+      vim.fn.setreg("+", url)
+      Snacks.notify(url, { title = "Git link copied" })
+    end,
+  })
+end
+
 function M.setup()
   require("snacks").setup({
     picker = {
@@ -14,6 +36,7 @@ function M.setup()
   vim.keymap.set("n", "<leader>f", picker.grep, { desc = "Snacks live grep (fuzzy content search)" })
   vim.keymap.set("n", "<leader>b", picker.buffers, { desc = "Snacks buffers (fuzzy switch)" })
   vim.keymap.set("n", "<leader>?", picker.help, { desc = "Snacks help tags" })
+  vim.keymap.set({ "n", "x" }, "<leader>gl", copy_git_link, { desc = "Copy git permalink to clipboard" })
 end
 
 return M
